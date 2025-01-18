51812b54768baca27b8382f0e1b600d9fb5e544e
sandip1401 <sandipgpay224@gmail.com>
2025-01-18 19:22:45 +0530

    error code

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
 const app=require('express')();
 const http=require('http').Server(app);
-
+require('dotenv').config();
+console.log("here is mongo",process.env.MONGODB_URL);
 const mongoose=require('mongoose');
 mongoose.connect("mongodb+srv://sandipgpay224:Sandip&14@cluster0.kvho0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-
 const Person=require('./models/userModel');
 
 const bodyParser = require("body-parser"); // Correct case
 app.use(bodyParser.json()); // Use the JSON parser middleware
 
-http.listen(3000,()=>{
-    console.log('server is running');
+const PORT=process.env.PORT;
+http.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
 })
 
-app.post('/person',async(req,res)=>{
-    try{
-        const data=req.body
-        const newPerson= new Person(data);
-        const response=await newPerson.save();
-        console.log('data saved');
-        res.status(200).json(response);
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({error:'internal server error'});
-    }
-})
-
-app.get('/person',async(req,res)=>{
-    try{
-        const data=await Person.find();
-        console.log('data fetch');
-        res.status(200).json(data);
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({error:'not found'});
-    }
-})
 
 const personroutes=require('./routes/personroutes');
 app.use('/',personroutes);
--- routes/personroutes.js
+++ routes/personroutes.js
@@ -2,6 +2,32 @@ const express=require('express');
 const person = require('../models/userModel');
 const router=express.Router();
 
+router.post('/person',async(req,res)=>{
+    try{
+        const data=req.body
+        const newPerson= new Person(data);
+        const response=await newPerson.save();
+        console.log('data saved');
+        res.status(200).json(response);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:'internal server error'});
+    }
+})
+
+router.get('/person',async(req,res)=>{
+    try{
+        const data=await Person.find();
+        console.log('data fetch');
+        res.status(200).json(data);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:'not found'});
+    }
+})
+
 router.put('/:id',async(req,res)=>{
     try{
         const personId=req.params.id;
