5919185d4041465528105498a573e4b040975568
Pritam-2002 <mukherjeep7654321@gmail.com>
2025-01-18 19:32:23 +0530

    hii

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-const app=require('express')();
-const http=require('http').Server(app);
-require('dotenv').config();
-console.log("here is mongo",process.env.MONGODB_URL);
-const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://sandipgpay224:Sandip&14@cluster0.kvho0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-const Person=require('./models/userModel');
+const app = require("express")();
+const http = require("http").Server(app);
+require("dotenv").config();
+console.log("here is mongo", process.env.MONGODB_URL);
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URL);
+const Person = require("./models/userModel");
 
 const bodyParser = require("body-parser"); // Correct case
 app.use(bodyParser.json()); // Use the JSON parser middleware
 
-const PORT=process.env.PORT;
-http.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`)
-})
+const PORT = process.env.PORT || 8000;
+http.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
 
-
-const personroutes=require('./routes/personroutes');
-app.use('/',personroutes);
-
-
-
+const personroutes = require("./routes/personroutes");
+app.use("/", personroutes);
